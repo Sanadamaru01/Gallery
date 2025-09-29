2432b789edf9854dbd1221ba7fd6cddd80fea5fd
Sanadamaru <48264954+Sanadamaru01@users.noreply.github.com>
2025-09-29 21:30:40 +0900

    Update cameraControls.js

--- js/cameraControls.js
+++ js/cameraControls.js
@@ -100,7 +100,7 @@ export function setupCameraControls(camera, renderer, controlsTargetY, floor, sc
       const panelHeight = panel.userData.size?.height || 1;  // パネル高さ
       const fixedLongSide = 3;                               // 基準高さ
       const baseDistance = -0.5;                              // 元の距離
-      const safetyMargin = -1.2;                              // マージン
+      const safetyMargin = -1.5;                              // マージン
       const distance = baseDistance * (panelHeight / fixedLongSide) + safetyMargin;
 
       console.log(distance, panelHeight)
